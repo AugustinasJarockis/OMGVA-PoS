13aa98fee8c73ca2c6317f0d13fc4b4a44fa63fd
Olen <olen2003@gmail.com>
2024-11-18 15:20:22 +0200

    Changed backend port to 9900

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5260', // Backend server
+        target: 'http://localhost:9900', // Backend server
         changeOrigin: true,
         secure: false, // Set to true if using HTTPS with valid certificates
         rewrite: (path) => path.replace(/^\/api/, ''),
