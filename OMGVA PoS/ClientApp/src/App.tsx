3148570abf5b56248c731ecaaa7cd413b536cdcc
gigl9232 <gita.gliaudyte@mif.stud.vu.lt>
2024-12-09 14:28:57 +0200

    mini fix

--- OMGVA PoS/ClientApp/src/App.tsx
+++ OMGVA PoS/ClientApp/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import BusinessPage from './pages/BusinessPage';
@@ -6,14 +6,38 @@ import SelectBusinessPage from './pages/SelectBusinessPage';
 import UpdateBusinessPage from './pages/UpdateBusinessPage';
 import WeatherPage from './pages/WeatherPage';
 import './App.css';
-import { getTokenRole } from './utils/tokenUtils';
+import { getTokenRole, isTokenValid } from './utils/tokenUtils';
 import CreateBusinessPage from './pages/CreateBusinessPage';
 
 const App: React.FC = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!localStorage.getItem('authToken'));
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    const checkTokenValidity = () => {
+        const token = localStorage.getItem('authToken');
+        if (token && isTokenValid(token)) {
+            setIsAuthenticated(true);
+        } else {
+            localStorage.removeItem('authToken');
+            setIsAuthenticated(false);
+        }
+    };
+
+    useEffect(() => {
+        checkTokenValidity();
+
+        const handleUnload = () => {
+            localStorage.removeItem('authToken');
+        };
+
+        window.addEventListener('beforeunload', handleUnload);
+
+        return () => {
+            window.removeEventListener('beforeunload', handleUnload);
+        };
+    }, []);
 
     const handleLoginSuccess = () => {
-        setIsAuthenticated(true);
+        checkTokenValidity();
     };
 
     const handleLogout = () => {
@@ -42,9 +66,9 @@ const App: React.FC = () => {
                             <Route path="/business/create" element={<CreateBusinessPage token={localStorage.getItem('authToken')} />} />
                             <Route path="/business/update/:id" element={<UpdateBusinessPage token={localStorage.getItem('authToken')} />} />
                             <Route path="/weather" element={<WeatherPage onLogout={handleLogout} />} />
-                                {localStorage.getItem('authToken') !== null && (getRole() === "Admin")
-                                    ? (<Route path="*" element={<Navigate to="/business" />} />)
-                                    : (<Route path="*" element={<Navigate to="/weather" />} />)}
+                            {localStorage.getItem('authToken') !== null && getRole() === "Admin"
+                                ? (<Route path="*" element={<Navigate to="/business" />} />)
+                                : (<Route path="*" element={<Navigate to="/weather" />} />)}
                         </>
                     )}
                 </Routes>
