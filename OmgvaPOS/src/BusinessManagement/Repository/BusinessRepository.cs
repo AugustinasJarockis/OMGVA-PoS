f96eaf4472359629da58088d36121b0bb891b67c
Augustinas <118769619+AugustinasJarockis@users.noreply.github.com>
2024-12-13 01:20:44 +0200

    Add business service (#30)
    Co-authored-by: “Augustinas” <jarauga@gmail.com>

--- OmgvaPOS/src/BusinessManagement/Repository/BusinessRepository.cs
+++ OmgvaPOS/src/BusinessManagement/Repository/BusinessRepository.cs
@@ -1,5 +1,6 @@
 ﻿using OmgvaPOS.AuthManagement.Repository;
 using OmgvaPOS.BusinessManagement.DTOs;
+using OmgvaPOS.BusinessManagement.Mappers;
 using OmgvaPOS.BusinessManagement.Models;
 using OmgvaPOS.Database.Context;
 
@@ -9,62 +10,27 @@ namespace OmgvaPOS.BusinessManagement.Repository
     {
         private readonly OmgvaDbContext _database = database;
         private readonly IAuthenticationRepository _authenticationRepository = authenticationRepository;
-        public BusinessDTO CreateBusiness(CreateBusinessRequest createBusinessRequest) {
-            Business business = new() {
-                StripeAccId = "/////////////////////////",//TODO: somehow acquire stripe acc id
-                Name = createBusinessRequest.Name,
-                Address = createBusinessRequest.Address,
-                Phone = createBusinessRequest.Phone,
-                Email = createBusinessRequest.Email
-            };
-
+        public List<Business> GetBusinesses() {
+            return [.. _database.Businesses];
+        }
+        public Business GetBusiness(long businessId) {
+            return _database.Businesses.Where(b => b.Id == businessId).FirstOrDefault()!;
+        }
+        public Business CreateBusiness(Business business) {
             _database.Businesses.Add(business);
             _database.SaveChanges();
 
-            return new () {
-                Id = business.Id,
-                Name = business.Name,
-                Address = business.Address,
-                Phone = business.Phone,
-                Email = business.Email
-            };
+            return business;
         }
-
-        public bool UpdateBusiness(long businessId, BusinessDTO business) {
-            var businessToUpdate = _database.Businesses.SingleOrDefault(b => b.Id == businessId);
-            if (businessToUpdate != null) {
-                businessToUpdate.Name = business.Name ?? businessToUpdate.Name;
-                businessToUpdate.Address = business.Address ?? businessToUpdate.Address;
-                businessToUpdate.Phone = business.Phone ?? businessToUpdate.Phone;
-                businessToUpdate.Email = business.Email ?? businessToUpdate.Email;
+        public bool UpdateBusiness(Business business) {
+            if (business != null) {
+                _database.Update(business);
                 _database.SaveChanges();
                 return true;
             }
             else {
                 return false;
             }
-        }
-
-        public List<BusinessDTO> GetBusinesses() {
-            return [.. _database.Businesses.Select<Business, BusinessDTO>(
-                b => new() {
-                    Id = b.Id,
-                    Name = b.Name,
-                    Address = b.Address,
-                    Phone = b.Phone,
-                    Email = b.Email
-                })]; ;
-        }
-
-        public BusinessDTO GetBusiness(long businessId) {
-            return _database.Businesses.Where(b => b.Id == businessId).Select<Business, BusinessDTO>(
-                b => new() {
-                    Id = b.Id,
-                    Name = b.Name,
-                    Address = b.Address,
-                    Phone = b.Phone,
-                    Email = b.Email
-                }).FirstOrDefault()!;
         }
     }
 }
---
+++ OmgvaPOS/src/BusinessManagement/Services/BusinessService.cs
@@ -0,0 +1,26 @@
+﻿using OmgvaPOS.BusinessManagement.DTOs;
+using OmgvaPOS.BusinessManagement.Mappers;
+using OmgvaPOS.BusinessManagement.Models;
+using OmgvaPOS.BusinessManagement.Repository;
+
+namespace OmgvaPOS.BusinessManagement.Services
+{
+    public class BusinessService(IBusinessRepository businessRepository) : IBusinessService
+    {
+        private readonly IBusinessRepository _businessRepository = businessRepository;
+        public List<BusinessDTO> GetBusinesses() {
+            return _businessRepository.GetBusinesses().Select(b => b.ToBusinessDTO()).ToList();
+        }
+        public BusinessDTO GetBusiness(long id) {
+            return _businessRepository.GetBusiness(id).ToBusinessDTO();
+        }
+        public BusinessDTO CreateBusiness(CreateBusinessRequest request) {
+            return _businessRepository.CreateBusiness(request.ToBusiness()).ToBusinessDTO();
+        }
+        public bool UpdateBusiness(BusinessDTO business) {
+            Business businessToUpdate = _businessRepository.GetBusiness((long)business.Id); //TODO: Unlikely but potential error here 
+            businessToUpdate = business.ToBusiness(businessToUpdate);
+            return _businessRepository.UpdateBusiness(businessToUpdate);
+        }
+    }
+}
