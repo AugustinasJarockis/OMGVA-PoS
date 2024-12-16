a56eba846f2ea21dff3730283e79508a2c599fc7
“Augustinas” <jarauga@gmail.com>
2024-12-16 16:06:16 +0200

    Add item management frontend

---
+++ OmgvaPOS/ClientApp/src/components/Forms/ItemDataForm/index.tsx
@@ -0,0 +1,157 @@
+﻿import React, { useEffect, useState } from 'react';
+import { ChangeableItemFields, Item } from '../../../services/itemService';
+import '../../../pages/Item pages/ItemPages.css';
+import UserSelector from '../../Selectors/UserSelector';
+
+interface ItemDataFormProps {
+    item?: Item;
+    itemGroup?: string;
+    submitText?: string;
+    onSubmit: (item: ChangeableItemFields) => void;
+    required?: boolean;
+}
+
+const ItemDataForm: React.FC<ItemDataFormProps> = (props: ItemDataFormProps) => {
+    const [isService, setIsService] = useState<boolean>(false);
+
+    const handleSubmission = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const formElements = form.elements as typeof form.elements & {
+            name: { value: string }
+            inventoryQuantity: { value: string }
+            price: { value: string }
+            currency: { value: string }
+            itemGroup: { value: string }
+            imgPath: { value: string }
+            discount: { value: string }
+            duration: { value: string }
+            userId: { value: string }
+        }
+
+        const itemInfo: ChangeableItemFields = {
+            Name: formElements.name.value === '' ? undefined : formElements.name.value,
+            InventoryQuantity: formElements.inventoryQuantity.value === '' ? undefined : +formElements.inventoryQuantity.value,
+            Price: formElements.price.value === '' ? undefined : +formElements.price.value,
+            Currency: formElements.currency.value === '' ? undefined : formElements.currency.value,
+            ItemGroup: props.itemGroup ? props.itemGroup : (formElements.itemGroup.value === '' ? undefined : formElements.itemGroup.value),
+            ImgPath: formElements.imgPath.value === '' ? undefined : formElements.imgPath.value,
+            DiscountId: formElements.discount.value === '' ? undefined : formElements.discount.value,
+            Duration: !formElements.duration || formElements.duration.value === '' ? undefined : ('0' + formElements.duration.value + ':00'),
+            UserId: !formElements.userId || formElements.userId.value === '' ? undefined : formElements.userId.value,
+        };
+
+        props.onSubmit(itemInfo);
+    }
+
+    const changeItemType = () => {
+        if (isService) {
+            setIsService(false);
+        }
+        else {
+            setIsService(true);
+        }
+        
+    }
+
+    useEffect(() => {
+        if (props.item?.UserId) {
+            setIsService(true);
+        }
+    }, []);
+
+    return (
+        <div>
+            <>
+                <form onSubmit={handleSubmission}>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name" placeholder={props.item?.Name} required={props.required} /><br /><br />
+                    <label htmlFor="inventoryQuantity">Inventory quantity</label>
+                    <input
+                        type="number"
+                        id="inventoryQuantity"
+                        name="inventoryQuantity"
+                        placeholder={props.item?.InventoryQuantity ? String(props.item?.InventoryQuantity) : undefined}
+                        required={props.required}
+                        min='0'
+                    /><br /><br />
+                    <label htmlFor="price">Price</label>
+                    <input
+                        type="text"
+                        id="price"
+                        name="price"
+                        placeholder={props.item?.Price ? String(props.item?.Price) : undefined}
+                        pattern="[0-9]+(.[0-9][0-9]?)?"
+                        required={props.required}
+                        onInvalid={e => e.currentTarget.setCustomValidity('Please enter a price.')}
+                        onInput={e => e.currentTarget.setCustomValidity('')}
+                    /><br /><br />
+                    <label htmlFor="currency">Currency</label>
+                    <input
+                        type="text"
+                        id="currency"
+                        name="currency"
+                        pattern="[a-zA-z]{3}"
+                        placeholder={props.item?.Currency}
+                        required={props.required}
+                        onInvalid={e => e.currentTarget.setCustomValidity('Please enter a currency type. The type has to consist of three letters.')}
+                        onInput={e => e.currentTarget.setCustomValidity('')}
+                    /><br /><br />
+                    {!props.itemGroup && (
+                        <>
+                            <label htmlFor="itemGroup">Item group</label>
+                            <input
+                                type="text"
+                                id="itemGroup"
+                                name="itemGroup"
+                                placeholder={props.item?.ItemGroup}
+                                required={props.required}
+                            /><br /><br />
+                        </>
+                        )
+                    }
+                    <label htmlFor="imgPath">Path to image</label>
+                    <input
+                        type="text"
+                        id="imgPath"
+                        name="imgPath"
+                        placeholder={props.item?.ImgPath}
+                        required={props.required}
+                    /><br /><br />
+                    <label htmlFor="discount">Discount</label>
+                    <input
+                        type="text"
+                        id="discount"
+                        name="discount"
+                        placeholder={props.item?.DiscountId}
+                    /><br /><br />
+                    <div className="mid-form-input">
+                        Is service:
+                        <input type="checkbox" onClick={changeItemType} checked={ isService } readOnly />
+                    </div><br />
+                    {isService &&
+                    <>
+                        <label htmlFor="duration">Duration</label>
+                        <input
+                            type="text"
+                            id="duration"
+                            name="duration"
+                            pattern="[0-7]{1}:[0-9]{2}"
+                            placeholder={props.item?.Duration}
+                            required={props.required}
+                            onInvalid={e => e.currentTarget.setCustomValidity('Please enter the duration in format H:mm:ss.')}
+                            onInput={e => e.currentTarget.setCustomValidity('')}
+                        /><br /><br />
+                        <label>Service provider</label>
+                        <UserSelector current={props.item?.UserId} required={props.required} /><br/><br/>
+                    </>
+                    }
+                    <input type="submit" value={props.submitText ? props.submitText : "Submit"} />
+                </form>
+            </>
+        </div>
+    );
+};
+
+export default ItemDataForm;
---
+++ OmgvaPOS/ClientApp/src/components/Selectors/UserSelector/index.tsx
@@ -0,0 +1,57 @@
+﻿import React, { useEffect, useState } from 'react';
+import './UserSelector.css';
+import { useAuth } from '../../../contexts/AuthContext';
+import { getBusinessUsers } from '../../../services/userService';
+import { getTokenBusinessId } from '../../../utils/tokenUtils';
+
+interface UserSelectorProps {
+    required?: boolean;
+    current?: string;
+}
+
+const UserSelector: React.FC<UserSelectorProps> = (props: UserSelectorProps) => {
+    const [error, setError] = useState<string | null>(null);
+    const [listItems, setListItems] = useState<Array<JSX.Element>>();
+    const { authToken } = useAuth();
+
+    const getUsers = async () => {
+        setError(null);
+
+        try {
+            const { result, error } = await getBusinessUsers(authToken, getTokenBusinessId(authToken ?? ''));
+
+            if (!result) {
+                setError('Problem acquiring user: ' + error);
+            }
+            else {
+                setListItems(result.map(user =>
+                    <option key={user.Id} value={user.Id}>
+                        { user.Name }
+                    </option>));
+            }
+        } catch (err: any) {
+            setError(err.message || 'An unexpected error occurred.');
+        }
+    }
+
+    useEffect(() => {
+        if (!authToken) {
+            setError("You have to authenticate first!");
+        }
+        else {
+            getUsers();
+        }
+    }, []);
+
+    return (
+        <div className="selector-container">
+            <select className="wider-select" name="userId" id="userId" required={props?.required} defaultValue={props.current ? props.current : '' }>
+                <option hidden disabled value=''> -- select an option -- </option>
+                {listItems}
+            </select>
+        </div>
+        || error
+    );
+};
+
+export default UserSelector;
