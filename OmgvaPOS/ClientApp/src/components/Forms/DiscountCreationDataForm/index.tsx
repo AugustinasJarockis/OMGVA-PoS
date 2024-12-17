cd31c4019facc71f808687291719d43a887d71e3
“Augustinas” <jarauga@gmail.com>
2024-12-17 12:30:03 +0200

    Add discount management frontend

---
+++ OmgvaPOS/ClientApp/src/components/Forms/DiscountCreationDataForm/index.tsx
@@ -0,0 +1,55 @@
+﻿import React from 'react';
+import { Discount, DiscountCreateRequest, DiscountType } from '../../../services/discountService';
+
+interface DiscountCreationDataFormProps {
+    discount?: Discount;
+    orderId?: string;
+    submitText?: string;
+    onSubmit: (discount: DiscountCreateRequest) => void;
+    type: DiscountType;
+}
+
+const DiscountCreationDataForm: React.FC<DiscountCreationDataFormProps> = (props: DiscountCreationDataFormProps) => {
+    const handleSubmission = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const formElements = form.elements as typeof form.elements & {
+            amount: { value: string }
+            timeValidUntil: { value: string }
+        }
+
+        const discountInfo: DiscountCreateRequest = {
+            Amount: +formElements.amount.value,
+            TimeValidUntil: formElements.timeValidUntil.value.replace(' ', 'T') + ':00',
+            Type: props.type,
+            OrderId: props.orderId
+        };
+
+        props.onSubmit(discountInfo);
+    }
+
+    return (
+        <div>
+            <>
+                <form onSubmit={handleSubmission}>
+                    <label htmlFor="amount">Amount</label>
+                    <input type="text" id="amount" name="amount" placeholder={props.discount?.Amount} required /><br /><br />
+                    <label htmlFor="timeValidUntil">Valid until</label>
+                    <input type="text"
+                        id="timeValidUntil"
+                        name="timeValidUntil"
+                        pattern="[0-9]{4}-(1[0-2]|0[1-9])-(0[1-9]|[1-2][0-9]|3[0-1]) ([0-1][0-9]|2[0-4]):[0-5][0-9]"
+                        placeholder={props.discount?.TimeValidUntil}
+                        required
+                        onInvalid={e => e.currentTarget.setCustomValidity('Please enter a date and time in format YYYY-MM-DD hh:mm.')}
+                        onInput={e => e.currentTarget.setCustomValidity('')}
+                    /><br /><br />
+                    <input type="submit" value={props.submitText ? props.submitText : "Submit"} />
+                </form>
+            </>
+        </div>
+    );
+};
+
+export default DiscountCreationDataForm;
--- OmgvaPOS/Migrations/20241216230428_sync-with-all.Designer.cs
+++ OmgvaPOS/Migrations/20241216230428_sync-with-all.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OmgvaPOS.Database.Context;
 
@@ -11,9 +12,11 @@ using OmgvaPOS.Database.Context;
 namespace OmgvaPOS.Migrations
 {
     [DbContext(typeof(OmgvaDbContext))]
-    partial class OmgvaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241216230428_sync-with-all")]
+    partial class syncwithall
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
