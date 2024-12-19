ce7d99ee13413cb6bc3ad149897f60f9e035eed5
gigl9232 <gita.gliaudyte@mif.stud.vu.lt>
2024-12-19 16:01:04 +0200

    Adding component for order split

---
+++ OmgvaPOS/ClientApp/src/components/Selectors/OrderItemSelector/OrderItemSelector.tsx
@@ -0,0 +1,40 @@
+﻿import React, { useState } from 'react';
+import { OrderItem } from '../../../services/orderItemService';
+
+interface OrderItemSelectorProps {
+    currency: string;
+  orderItems: Array<OrderItem>;
+  onSelectionChange: (selectedItems: Array<string>) => void;
+}
+
+const OrderItemSelector: React.FC<OrderItemSelectorProps> = ({ currency, orderItems, onSelectionChange }) => {
+  const [selectedItems, setSelectedItems] = useState<Array<string>>([]);
+
+  const handleCheckboxChange = (itemId: string, isChecked: boolean) => {
+    const updatedSelectedItems = isChecked
+      ? [...selectedItems, itemId]
+      : selectedItems.filter(id => id !== itemId);
+
+    setSelectedItems(updatedSelectedItems);
+    onSelectionChange(updatedSelectedItems);
+  };
+
+  return (
+    <div className="order-item-selector">
+      {orderItems.map(item => (
+        <div key={item.Id} className="order-item">
+          <label>
+            <input
+              type="checkbox"
+              checked={selectedItems.includes(item.Id)}
+              onChange={(e) => handleCheckboxChange(item.Id, e.target.checked)}
+                  />
+                  {item.ItemName} - {item.TotalPrice} {currency}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default OrderItemSelector;
